981646cbf0a8dd5d0205a01f3f260ef8e6c13d10
nkd920 <matsumura.tomoe920@gmail.com>
2019-01-20 22:29:56 +0900

    Initial commit

---
+++ components/AppHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <nav class="navbar">
+      <div class="container">
+        <div class="navbar-brand">
+          <nuxt-link to="/breeds" class="navbar-item">ASMR Search</nuxt-link>
+          <span class="navbar-burger burger" data-target="navbarMenu">
+            <span></span> <span></span> <span></span>
+          </span>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div><AppHeader></AppHeader> <nuxt /></div>
+</template>
+
+<script>
+import AppHeader from "@/components/AppHeader.vue";
+export default {
+  components: {
+    AppHeader
+  }
+};
+</script>
+
+<style></style>
